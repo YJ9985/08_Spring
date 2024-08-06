7ea5b6ae3d8ab29ae288cf98d02a423bdd7e92aa
YJ9985 <lucy1486@naver.com>
2024-08-06 18:38:30 +0900

    240806 오후수업

---
+++ afternoon/src/main/java/org/example/controller/todo/TodoSaveControllerV1.java
@@ -0,0 +1,27 @@
+package org.example.controller.todo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.controller.dto.todo.TodoDtoListV1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@Slf4j
+public class TodoSaveControllerV1 {
+    private TodoDtoListV1 todoList = TodoDtoListV1.getInstance();
+
+    @RequestMapping(value = "todo/form/save", method = RequestMethod.GET)
+    public String addSave(HttpServletRequest request) {
+        log.info("==========> 회원 추가 Request 호출, /todo/form/save");
+
+        String todo = request.getParameter("todo");
+
+        todoList.addList(todo);
+
+        request.setAttribute("todoList", todoList.getList());
+        return "todo-show";
+    }
+}
---
+++ afternoon/src/main/java/org/example/controller/todo/TodoShowControllerV1.java
@@ -0,0 +1,23 @@
+package org.example.controller.todo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.controller.dto.todo.TodoDtoListV1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@Slf4j
+public class TodoShowControllerV1 {
+    private TodoDtoListV1 todoDtoList = TodoDtoListV1.getInstance();
+
+    @GetMapping("/todo/show")
+    public String process(HttpServletRequest request) {
+        log.info("==============> Todo 목록 조회 페이지 호출, /todo/show");
+
+        request.setAttribute("todoList", todoDtoList.getList());
+        return "todo-show";
+    }
+}
